ad2595f118999ca57fbe614aefbc1eb7d1c8a8b7
Vojtech Vzplel <vojvyp@gmail.com>
2021-09-30 12:27:54 +0200

    up

--- Lab2/Src/main.c
+++ Lab2/Src/main.c
@@ -20,17 +20,165 @@
 #include <stdint.h>
 #include "stm32f0xx.h"
 
+volatile  uint32_t Tick;
+
+
+#define LED_TIME_BLINK 300
+
+#define LED_TIME_SHORT 100
+
+#define LED_TIME_LONG 1000
+
+#define BUTTON_TIME_DELAY 40
+
+#define BUTTON_TIME_DELAY_ADVANCED 5
+
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+
+void EXTI0_1_IRQHandler(void)
+{
+	if (EXTI->PR & EXTI_PR_PR0)
+	{
+		EXTI->PR |= EXTI_PR_PR0;
+		GPIOB->ODR ^= (1<<0);
+	}
+}
+
+/*SysTick*/
+void SysTick_Handler(void)
+{
+	Tick++;
+}
+
+/*LED blink*/
+
+void blink(void)
+{
+	static uint32_t delay;
+
+	if (Tick > delay + LED_TIME_BLINK)
+	{
+		GPIOA->ODR ^= (1<<4);
+		delay = Tick;
+	}
+}
+
+void tlacitko (void)
+{
+	static uint32_t delay_tlacitko; //delay 40ms
+
+	static uint32_t off_time; // counting off_time
+
+	if (Tick > delay_tlacitko + BUTTON_TIME_DELAY)
+	{
+		static uint32_t old_s1;
+
+		static uint32_t old_s2;
+
+		uint32_t new_s1 = GPIOC->IDR & (1<<1); // button B1
+
+		uint32_t new_s2 = GPIOC->IDR & (1<<0); // button B2
+
+
+		if (old_s1 && !new_s1) // falling edge
+		{
+			off_time = Tick + LED_TIME_SHORT;
+			GPIOB->BSRR = (1<<0);
+		}
+		old_s1 = new_s1;
+
+
+		if (old_s2 && !new_s2) // falling edge
+		{
+			off_time = Tick + LED_TIME_LONG;
+			GPIOB->BSRR = (1<<0);
+		}
+		old_s2 = new_s2;
+
+
+		if (Tick > off_time)
+		{
+		GPIOB->BRR = (1<<0);
+		}
+
+		delay_tlacitko = Tick;
+	}
+}
+
+/*Advanced debouncing*/
+
+void button (void)
+{
+	static uint32_t delay_tlacitko; // delay 40ms
+	static uint32_t delay_button_advanced; // delay 5ms
+	static uint32_t off_time; //counting off_time
+
+	if (Tick > delay_button_advanced + BUTTON_TIME_DELAY_ADVANCED)
+	{
+		static uint16_t debounce = 0xFFFF; // 1
+
+		debounce <<=1;                    // LSB  0
+
+		if (GPIOC->IDR & (1<<1)) debounce |= 0x0001;
+		if (debounce == 0x8000)
+		{
+			off_time = Tick + LED_TIME_LONG;
+			GPIOB->BSRR = (1<<0);
+		}
+	}
+
+	if (Tick > delay_tlacitko + BUTTON_TIME_DELAY)
+	{
+		static uint32_t old_s2; // for comparing the button
+		uint32_t new_s2 = GPIOC->IDR & (1<<0); // push B2
+
+		if (old_s2 && !new_s2) // falling edge
+		{
+			off_time = Tick + LED_TIME_SHORT;
+			GPIOB->BSRR = (1<<0);
+		}
+		old_s2 = new_s2;
+
+		if (Tick > off_time)
+		{
+		GPIOB->BRR = (1<<0);
+		}
+
+		delay_tlacitko = Tick;
+	}
+}
+
+
 int main(void)
 {
 
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable
+	SysTick_Config(8000); // 1ms SysTick Timer
+
+	/*Enable registers and set LEDs and PullUps*/
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable register GPIO A to B
 	GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
-	GPIOB->MODER |= GPIO_MODER_MODER; // LED2 = PB0, output
+	GPIOB->MODER |= GPIO_MODER_MODER0_0; // LED2 = PB0, output
 	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0; // S2 = PC0, pullup
-	GPIOC->PUPDR |= GPIO_..............; // S1 = PC1, pullup
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
+
+	/* Enable Clock for SYSCFG*/
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+
+	/* Choose Interupt source, allow mask and choose falling edge*/
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	EXTI->IMR |= EXTI_IMR_MR0; // mask
+	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+	// NVIC_EnableIRQ(EXTI0_1_IRQn); // enable interupt request for EXTI0_1
+
+	while(1){
+		blink();
+		tlacitko();
+	}
 
 }
